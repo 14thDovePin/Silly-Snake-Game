5f610ed661c198f58df50700e0994e4f12b8b7f0
14thDovePin <therlaf0409@gmail.com>
2024-07-10 15:13:03 +0800

    Implemented Pygame
    - Created a todo list.
    - Implemented pygame at a very basic level.

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    pass
+import pygame
+
+
+RESOLUTION = (1280, 720)
+UPS = 60
+
+
+class Game:
+    def __init__(self):
+        """Initialize Pygame"""
+        pygame.init()
+        self.screen = pygame.display.set_mode(RESOLUTION)
+        self.clock = pygame.time.Clock()
+        self.cycle = True
+
+    def start(self):
+        # Limit Updates per Second
+        self.clock.tick(UPS)
+        while self.cycle:
+            pass
 
 
 if __name__ == '__main__':
-    main()
+    Game().start()
