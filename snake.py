58fed4de8caf1047a79970c5995ea4362f8c7945
14thDovePin <therlaf0409@gmail.com>
2024-07-12 01:23:40 +0800

    Added automatic movement and direction control to snake.
    - added direction control to snake
    - added automatic movement to snake
    - added instant turn direction for more feel of control
    - fixed snake bug that lets you turn 180°
    - refactored code.

--- snake.py
+++ snake.py
@@ -5,48 +5,81 @@ class Snake:
     """The snake of the game!"""
 
     def __init__(self, screen):
-        self.screen = screen
-        self.dt = 0
-        self.tps = 1.2 # tiles per second
-        self.pos = Vector2(
-            self.screen.get_width()/2,
-            self.screen.get_height()/2
-        )
-        self.rect = Rect(
+        self._screen = screen
+        self._dt = 0
+        self.tile_size = 45  # in pixels
+        self.move_speed = 6  # tiles per second
+        self._tps = 1/self.move_speed
+        self.direction = 'n'  # north south east west
+        self._lock_direction = False
+
+        self._rect = Rect(
             (0, 0),  # Left Top
             (45, 45)  # Width Height
         )
 
+        self.pos = Vector2(
+            self._screen.get_width()/2,
+            self._screen.get_height()/2
+        )
+
         # Center object to screen.
-        self.rect.centerx = self.pos.x
-        self.rect.centery = self.pos.y
+        self._rect.centerx = self.pos.x
+        self._rect.centery = self.pos.y
 
     def input(self, key):
         """input updates"""
-        # basi movments
-        if key == K_w or key == K_UP:
-            self.pos.y -= 45
-        if key == K_s or key == K_DOWN:
-            self.pos.y += 45
-        if key == K_a or key == K_LEFT:
-            self.pos.x -= 45
-        if key == K_d or key == K_RIGHT:
-            self.pos.x += 45
+        # basic movments
+        if not self._lock_direction \
+        and (key == K_w or key == K_UP) \
+        and not self.direction == 's':
+            self._lock_direction = True
+            self.direction = 'n'
+
+        if not self._lock_direction \
+        and (key == K_s or key == K_DOWN) \
+        and not self.direction == 'n':
+            self._lock_direction = True
+            self.direction = 's'
+
+        if not self._lock_direction \
+        and (key == K_a or key == K_LEFT) \
+        and not self.direction == 'e':
+            self._lock_direction = True
+            self.direction = 'w'
+
+        if not self._lock_direction \
+        and (key == K_d or key == K_RIGHT) \
+        and not self.direction == 'w':
+            self._lock_direction = True
+            self.direction = 'e'
 
 
     def update(self, dt):
         """objet updates"""
         # Automatic Movement
-        self.dt += dt
-        if self.dt > 1:
-            self.pos.x += 45
-            self.dt -= 1
-        self.rect.center = (self.pos.x, self.pos.y)
+        self._dt += dt
+        if self._dt > self._tps:
+            self._move()
+            self._dt -= self._tps
+        self._rect.center = (self.pos.x, self.pos.y)
+
+    def _move(self):
+        """move forward in current direction"""
+        self._lock_direction = False
+        if self.direction == 'n':
+            self.pos.y -= self.tile_size
+        if self.direction == 's':
+            self.pos.y += self.tile_size
+        if self.direction == 'e':
+            self.pos.x += self.tile_size
+        if self.direction == 'w':
+            self.pos.x -= self.tile_size
 
     def draw(self):
         """"""
         draw.rect(
-            self.screen,
+            self._screen,
             (100, 100, 100),
-            self.rect,
+            self._rect,
         )
